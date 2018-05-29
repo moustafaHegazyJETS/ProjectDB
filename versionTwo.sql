3b2477b6d1809190b6520eef34509bf3f9f6f5e9
moustafaHegazyJETS <moustafahegazy123@gmail.com>
2018-05-29 15:15:01 +0200

    Trip-Version

--- versionTwo.sql
+++ versionTwo.sql
@@ -41,7 +41,7 @@ CREATE TABLE `driver_car_info` (
   UNIQUE KEY `driveCarID_UNIQUE` (`driveCarID`),
   UNIQUE KEY `UserId_UNIQUE` (`UserId`),
   CONSTRAINT `UserIdFK` FOREIGN KEY (`UserId`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +50,64 @@ CREATE TABLE `driver_car_info` (
 
 LOCK TABLES `driver_car_info` WRITE;
 /*!40000 ALTER TABLE `driver_car_info` DISABLE KEYS */;
+INSERT INTO `driver_car_info` VALUES (3,'moustafa','123456789','portsaid','2019-01-01','nissan','gol',1994,2500,'red',38,'a','a','1');
 /*!40000 ALTER TABLE `driver_car_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trip`
+--
+
+DROP TABLE IF EXISTS `trip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trip` (
+  `idTrip` int(11) NOT NULL AUTO_INCREMENT,
+  `tripName` varchar(45) NOT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  `driverId` int(11) NOT NULL,
+  `time` varchar(45) NOT NULL,
+  `from` varchar(60) NOT NULL,
+  `to` varchar(60) NOT NULL,
+  `numberOfSeats` int(11) NOT NULL,
+  PRIMARY KEY (`idTrip`),
+  KEY `tripdriver_idx` (`driverId`),
+  CONSTRAINT `tripdriver` FOREIGN KEY (`driverId`) REFERENCES `driver_car_info` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trip`
+--
+
+LOCK TABLES `trip` WRITE;
+/*!40000 ALTER TABLE `trip` DISABLE KEYS */;
+INSERT INTO `trip` VALUES (4,'visit','moustafa',38,'2002-00-00 02:00:00','portsaid','alex',4);
+/*!40000 ALTER TABLE `trip` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trip_reservation`
+--
+
+DROP TABLE IF EXISTS `trip_reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trip_reservation` (
+  `trip_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`trip_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trip_reservation`
+--
+
+LOCK TABLES `trip_reservation` WRITE;
+/*!40000 ALTER TABLE `trip_reservation` DISABLE KEYS */;
+INSERT INTO `trip_reservation` VALUES (4,37);
+/*!40000 ALTER TABLE `trip_reservation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -67,13 +124,13 @@ CREATE TABLE `user` (
   `E_Mail` varchar(45) NOT NULL,
   `gender` varchar(6) NOT NULL,
   `Mobile` varchar(12) NOT NULL,
-  `Birth_Date` date NOT NULL,
+  `Birth_Date` varchar(45) NOT NULL,
   `User_photo` varchar(100) NOT NULL,
   `National_id` varchar(14) NOT NULL,
   `pending` varchar(45) NOT NULL,
   PRIMARY KEY (`idUser`),
   UNIQUE KEY `E_Mail_UNIQUE` (`E_Mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,7 +139,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Moustafa','123','Moustafa@gmail.com','male','0125910002','1994-01-01','a','29408220300518','1'),(2,'hegayz','123','Moustafaa@gmail.com','male','01208475204','1994-01-01','a','29408220300518','0'),(3,'ele','123','sasaasas','male','012598415','1994-01-01','a','24564545161554','0'),(4,'dddd','123','asddd','male','0125594949','1994-01-01','a','29408220300518','0');
+INSERT INTO `user` VALUES (35,'moustafa','1234','Moustafa@gmail.com','male','01259100002','1994-01-01','a','29408220300518','2'),(36,'hegazy','1234','h@gmail.com','male','01259100002','1994-01-01','a','29408220300518','1'),(37,'s','1234','s@gmail.com','male','01259100002','1994-01-01','a','29408220300518','-1'),(38,'aa','1234','a@gmail.com','male','01259100002','1994-01-01','a','29408220300518','-1'),(39,'ddddd','12345','aaaaa@gmail.com','Male','01013362851','30/5/2018','aaa','29502081300563','0');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-29 16:48:53
+-- Dump completed on 2018-05-29 15:14:16
