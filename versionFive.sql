61b82450ec52955757969a9cce8ca6276f2d4f6e
moustafaHegazyJETS <moustafahegazy123@gmail.com>
2018-06-03 02:05:59 +0200

    version-3-6-1

--- versionFive.sql
+++ versionFive.sql
@@ -41,7 +41,7 @@ CREATE TABLE `driver_car_info` (
   UNIQUE KEY `driveCarID_UNIQUE` (`driveCarID`),
   UNIQUE KEY `UserId_UNIQUE` (`UserId`),
   CONSTRAINT `UserIdFK` FOREIGN KEY (`UserId`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +50,7 @@ CREATE TABLE `driver_car_info` (
 
 LOCK TABLES `driver_car_info` WRITE;
 /*!40000 ALTER TABLE `driver_car_info` DISABLE KEYS */;
-INSERT INTO `driver_car_info` VALUES (3,'moustafa','123456789','portsaid','2019-01-01','nissan','gol',1994,2500,'red',38,'a','a','1');
+INSERT INTO `driver_car_info` VALUES (3,'moustafa','123456789','portsaid','2019-01-01','nissan','gol',1994,2500,'red',38,'a','a','1234'),(4,'hegazy','123456789','portsaid','2019-01-01','nisaaan','gool',1994,2500,'blue',40,'a','a','1258'),(5,'sayed','123456799','alex','2019-01-01','ssssss','ssss',994,2500,'gold',41,'a','a','8888');
 /*!40000 ALTER TABLE `driver_car_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,15 +66,16 @@ CREATE TABLE `trip` (
   `tripName` varchar(45) NOT NULL,
   `details` varchar(100) DEFAULT NULL,
   `driverId` int(11) NOT NULL,
-  `time` varchar(45) NOT NULL,
-  `from` varchar(60) NOT NULL,
-  `to` varchar(60) NOT NULL,
+  `ttime` varchar(45) NOT NULL,
+  `tfrom` varchar(60) NOT NULL,
+  `tto` varchar(60) NOT NULL,
   `numberOfSeats` int(11) NOT NULL,
   `cost` float NOT NULL,
+  `day` varchar(45) NOT NULL,
   PRIMARY KEY (`idTrip`),
   KEY `tripdriver_idx` (`driverId`),
   CONSTRAINT `tripdriver` FOREIGN KEY (`driverId`) REFERENCES `driver_car_info` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,7 +84,7 @@ CREATE TABLE `trip` (
 
 LOCK TABLES `trip` WRITE;
 /*!40000 ALTER TABLE `trip` DISABLE KEYS */;
-INSERT INTO `trip` VALUES (4,'visit','moustafa',38,'02:00','portsaid','alex',1,19.5);
+INSERT INTO `trip` VALUES (4,'visit','moustafa',38,'18:48','portsaid','alex',0,19.5,'01/06/2018'),(5,'family','meet at portfouad',40,'14:30','portsaid','alex',3,20,'04/03/2018'),(6,'try','ssssss',41,'15:00','alex','portsaid',3,50,'01/02/2018'),(7,'try2','qqqqq',40,'20:00','alex','portsaid',3,50,'01/02/2018'),(8,'aa','aa',38,'20:00','a','a',2,20,'01/01/2019'),(9,'aa','aa',38,'20:00','a','a',2,20,'01/01/2019'),(10,'aa','aa',38,'20:00','a','a',1,20,'01/01/2019');
 /*!40000 ALTER TABLE `trip` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,7 +108,7 @@ CREATE TABLE `trip_reservation` (
 
 LOCK TABLES `trip_reservation` WRITE;
 /*!40000 ALTER TABLE `trip_reservation` DISABLE KEYS */;
-INSERT INTO `trip_reservation` VALUES (4,37);
+INSERT INTO `trip_reservation` VALUES (4,37),(4,38),(10,38),(5,40);
 /*!40000 ALTER TABLE `trip_reservation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,7 +132,7 @@ CREATE TABLE `user` (
   `pending` varchar(45) NOT NULL,
   PRIMARY KEY (`idUser`),
   UNIQUE KEY `E_Mail_UNIQUE` (`E_Mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,7 +141,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (35,'moustafa','1234','Moustafa@gmail.com','male','01259100002','1994-01-01','a','29408220300518','2'),(36,'hegazy','1234','h@gmail.com','male','01259100002','1994-01-01','a','29408220300518','1'),(37,'s','1234','s@gmail.com','male','01259100002','1994-01-01','a','29408220300518','-1'),(38,'aa','1234','a@gmail.com','male','01259100002','1994-01-01','a','29408220300518','1'),(39,'ddddd','12345','aaaaa@gmail.com','Male','01013362851','1994-01-01','aaa','29502081300563','1');
+INSERT INTO `user` VALUES (35,'moustafa','1234','Moustafa@gmail.com','male','01259100002','1994-01-01','a','29408220300518','2'),(36,'hegazy','1234','h@gmail.com','male','01259100002','1994-01-01','a','29408220300518','1'),(37,'s','1234','s@gmail.com','male','01259100002','1994-01-01','a','29408220300518','-1'),(38,'aa','1234','a@gmail.com','male','01259100002','1994-01-01','a','29408220300518','1'),(39,'ddddd','12345','aaaaa@gmail.com','Male','01013362851','1994-01-01','aaa','29502081300563','1'),(40,'ahmed','1234','ahmed@gmail.com','male','01020303040','1994-01-01','a','29408220300518','1'),(41,'sayed','1234','sayed@gmail.com','male','01202020202','1994-01-01','a','29408220300518','1'),(42,'mazen','1234','mazen@gmail.com','male','012591000002','1994-01-01','a','29408220300518','1');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-31 23:16:25
+-- Dump completed on 2018-06-03  2:05:30
